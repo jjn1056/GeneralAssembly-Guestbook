ec44a2d9af729310266bf1d319221439a2662645
John Napiorkowski <jjn1056@yahoo.com>
2011-12-08 12:28:59 -0500

    initial commit

---
+++ lib/GeneralAssembly/Guestbook.pm
@@ -0,0 +1,38 @@
+package GeneralAssembly::Guestbook;
+
+use 5.008008;
+use strict;
+use warnings FATAL =>'all';
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+GeneralAssembly::Guestbook - My Brand New Module
+
+=head1 SYNOPSIS
+
+  use GeneralAssembly::Guestbook;
+
+=head1 DESCRIPTION
+
+GeneralAssembly::Guestbook is ...
+
+=head1 AUTHOR
+
+John Napiorkowski L<email:jjnapiork@cpan.org>
+
+=head1 SEE ALSO
+
+    TBD
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, John Napiorkowski L<email:jjnapiork@cpan.org>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/use.t
@@ -0,0 +1,3 @@
+use Test::Most tests => 1;
+
+BEGIN { use_ok 'GeneralAssembly::Guestbook' }
