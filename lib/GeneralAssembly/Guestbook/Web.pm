ba98f2ec92b305395d8cf72a84ea819e95379046
John Napiorkowski <jjn1056@yahoo.com>
2011-12-16 14:10:46 -0500

    more slight refactor

--- lib/GeneralAssembly/Guestbook/Web.pm
+++ lib/GeneralAssembly/Guestbook/Web.pm
@@ -10,14 +10,17 @@ sub show_page {
   [200, ['Content-type'=>'text/html'], [$body]];
 }
 
+sub post_and_show_page {
+  (my $self = shift)->message_log
+   ->create_and_add_entry_if_valid(shift);
+  $self->show_page;
+}
+
 sub dispatch_request {
   sub(/) {
     sub(GET) { shift->show_page },
     sub (POST + %*) {
-      my ($self, $params) = @_;
-      $self->message_log
-       ->create_and_add_entry_if_valid($params);
-      $self->show_page;
+      shift->post_and_show_page(@_);
     }
   }
 }
